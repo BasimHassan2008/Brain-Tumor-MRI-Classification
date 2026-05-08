4280c50767726fee35e1aa06acc4d5c9906afe6e
BASIM HASSAN <167853212+BasimHassan2008@users.noreply.github.com>
2026-05-08 23:32:11 +0530

    Delete Model_Outputs directory

--- Model_Outputs/78e18c42_code.py
+++
@@ -1,6 +0,0 @@
-# Train the model
-history = model.fit(
-    train_generator,
-    epochs=15, # You can adjust the number of epochs
-    validation_data=test_generator
-)
--- Model_Outputs/fd94df8f_code.py
+++
@@ -1,3 +0,0 @@
-loss, accuracy = model.evaluate(test_generator)
-print(f"Test Loss: {loss:.4f}")
-print(f"Test Accuracy: {accuracy:.4f}")
